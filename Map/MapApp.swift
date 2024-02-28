64292bcf83ed9db01ba2062114344930bb22e917
allynefernanda <fernandavsob@gmail.com>
2024-02-28 13:02:50 -0300

    Initial Commit

---
+++ Map/MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapApp.swift
+//  Map
+//
+//  Created by allyne fernanda  on 28/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
